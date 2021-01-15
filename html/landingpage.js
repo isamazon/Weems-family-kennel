4e121ca9e40d2b0e820de8360668de7a43c4c656
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2021-01-15 13:24:17 -0800

    Adjustments to CSS
    > CSS: made some adjustments to page 1 on the landingpage to make it more responsive
    Added page2 and some content within it

---
+++ html/landingpage.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//  Object to contain all the landing page elements
+const landingPage = {
+  page1: document.querySelector(".page1"),
+  recipeBtn: document.getElementById("recipebtn"),
+  locationBtn: document.getElementById("locationbtn"),
+  guranteeBtn: document.getElementById("guranteebtn"),
+  contactBtn: document.getElementById("contactbtn"),
+};
+
+// Create an onclick function for the Recipe button to scroll to page2
