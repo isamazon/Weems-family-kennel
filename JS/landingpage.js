5ea70376a3b18d38297c2f0f5b3a5ce231fec2fe
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2021-01-20 14:57:49 -0800

    Refactored some html and CSS
    >HTML: Got rid of original image slider html and started a new one with radio-btns
    >CSS: Added some responsiveness
    
    > JS: added a page up function on page2

--- JS/landingpage.js
+++ JS/landingpage.js
@@ -8,10 +8,16 @@ const landingPage = {
   locationBtn: document.getElementById("locationbtn"),
   guranteeBtn: document.getElementById("guranteebtn"),
   contactBtn: document.getElementById("contactbtn"),
+  scrollUp1: document.getElementById("scrollUp"),
+  scrollDown1: document.getElementById("scrollDown"),
 };
 
 // Create an onclick function for the Recipe button to scroll to page2
 landingPage.recipeBtn.addEventListener("click", () => {
-  console.log("works");
   landingPage.page2.scrollIntoView({ behavior: "smooth", block: "center" });
+});
+
+// Create a onclick function to scroll up and below page2
+landingPage.scrollUp1.addEventListener("click", () => {
+  landingPage.page1.scrollIntoView({ behavior: "smooth", block: "center" });
 });
