45fdc7e73e870ace51900a6231c088258bcfacf5
Isaiah Mazon <37008425+isamazon@users.noreply.github.com>
2021-01-19 16:36:58 -0800

    Added some CSS animations and Added AOS library
    Added the AOS library for animation
     on scroll purposes.

--- html/landingpage.js
+++ html/landingpage.js
@@ -3,6 +3,7 @@
 //  Object to contain all the landing page elements
 const landingPage = {
   page1: document.querySelector(".page1"),
+  page2: document.querySelector(".page2"),
   recipeBtn: document.getElementById("recipebtn"),
   locationBtn: document.getElementById("locationbtn"),
   guranteeBtn: document.getElementById("guranteebtn"),
@@ -10,3 +11,7 @@ const landingPage = {
 };
 
 // Create an onclick function for the Recipe button to scroll to page2
+landingPage.recipeBtn.addEventListener("click", () => {
+  console.log("works");
+  page1.scrollIntoView({ behavior: "smooth", block: "center" });
+});
