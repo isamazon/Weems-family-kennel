0948636ac6b390914016006cbb98f775d65e680a
isamazon <37008425+isamazon@users.noreply.github.com>
2021-02-03 15:13:15 -0600

    Refactored the entire landing page
    Redoing the entire landing page using bootstrap 5 to make it fully responsive

--- JS/productCard.js
+++ JS/productCard.js
@@ -21,3 +21,20 @@ var popupViews = document.querySelectorAll('.popup-view');
         });
       });
     });
+
+    //Java-script for changing the background depending on if the image is a girl or boy 
+    // First we want to add object that contain a boy or girl variant inside the img id
+
+    let girl = document.getElementById('girlImage');
+    let boy = document.getElemenyById('boyImage');
+      // Add a background var
+    let gBackground = document.querySelector('.product-img')
+    
+
+    //Create a for each loop for the girl and boy object, changing the background style varying on the gender  
+    function changeBackground() {
+      for (i = 0; i <= boy.length; i++) {
+        genderBackground.gBackground.style.backgroundColor = 'blue';
+      }
+    }
+    changeBackground();
